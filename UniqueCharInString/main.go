98d5fcf40a3e9a4cb693d769aee8ecb1d14793f5
sreehari <sreeharirajesh1234@gmail.com>
2025-05-21 10:10:33 +0530

    leetcode-387

---
+++ UniqueCharInString/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func firstUniqChar(s string) int {
+
+	seen := make(map[rune]int)
+
+	for _, val := range s {
+		seen[val]++
+	}
+
+	for i, val := range s {
+		if seen[val] == 1 {
+			return i
+		}
+	}
+	return -1
+}
+
+func main() {
+
+	str := "aabb"
+
+	fmt.Println(firstUniqChar(str))
+}
