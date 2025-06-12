a1f621299f5d262b0f15fc9ba4bd01a9e17e9eb3
sreehari <sreeharirajesh1234@gmail.com>
2025-06-12 10:19:34 +0530

    sum of pairs

---
+++ Leetcode561/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func arrayPairSum(nums []int) int {
+	sort.Ints(nums)
+	sum := 0
+	for i := 0; i < len(nums); i += 2 {
+		sum += nums[i]
+	}
+	return sum
+}
+
+func main() {
+	nums := []int{6, 2, 6, 5, 1, 2}
+
+	result := arrayPairSum(nums)
+	fmt.Println(result)
+}
