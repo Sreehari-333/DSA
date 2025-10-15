5f6699fa2e5d150d543a5f987bf7125c4d9b2cea
Sreehari Rajesh <sreeharirajesh1234@gmail.com>
2025-10-15 15:37:33 +0530

    Time: 0 ms (100%), Space: 5.6 MB (33.33%) - LeetHub

---
+++ 4058-compute-alternating-sum/4058-compute-alternating-sum.go
@@ -0,0 +1,11 @@
+func alternatingSum(nums []int) int {
+    count := 0
+    for i := 0; i < len(nums); i++ {
+        if i%2 == 0 {
+            count += nums[i]
+        } else {
+            count -= nums[i]
+        }
+    }
+    return count
+}
